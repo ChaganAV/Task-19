2e1f9b6f3425787584904d70dd8f7def9dbadeb0
Alexandr <jwchaganav@gmail.com>
2022-10-21 15:33:10 +0400

    end

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите 5-ти значное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num/10000 == num%10)
+{
+    if ((num/1000)%10 == (num%100)/10)
+        Console.WriteLine($"Число {num} палиндром");
+    else
+        Console.WriteLine($"Число {num} не палиндром");
+}
+else
+    Console.WriteLine($"Число {num} не палиндром");
+
